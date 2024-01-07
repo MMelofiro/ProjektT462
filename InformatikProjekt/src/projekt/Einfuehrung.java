c1c816d4f10be8178af5561675b526776de42977
Marcello <110632107+MMelofiro@users.noreply.github.com>
2024-01-07 18:23:35 +0100

    now
    + Spiel 02 completed.
    + Method in Player - statusZeigen() - to show player's status.
    + Methods in Player class that allows to have checkpoints.
    + Method in Input class called inputDouble(), that allows us to ask for a double type value.
    + Method in Input - next() and fixNext() - to pause the game until the player presses a key, for better readability.

---
+++ InformatikProjekt/src/projekt/Einfuehrung.java
@@ -0,0 +1,18 @@
+package projekt;
+
+public class Einfuehrung { // Marcello Melofiro
+	
+	Input input = new Input();
+	
+	public void intro() {
+		
+		System.out.println("Here comes the introduction. I was thinking about telling the story, "
+				+ "some dialogue here and there, and maybe some choices for the player, "
+				+ "unimportant to the final ending, just to make this part feel less "
+				+ "like reading a book haha");
+		
+		input.next();
+		
+	}
+
+}
---
+++ InformatikProjekt/src/projekt/Hauptklasse.java
@@ -0,0 +1,90 @@
+package projekt;
+
+/*Hauptklasse Class
+ *	How is this class used?
+ * 		Instances/Objects of the other classes, will be created and used in the main method.
+ * 		If possible, no other methods will be created in this class.
+ * 		Declare the instances starting with lowercases.
+ * 
+ *	What is inside this class?
+ * 		Main method
+ *			introduction (method called)
+ *			character selection (method called)
+ *			difficulty selection (method called)
+ *			All the games  (method called)
+ *			Ending  (method called)
+ *			save methodes before and after each minigame
+ */
+
+public class Hauptklasse { // Marcello Melofiro
+	
+	public static void main(String[] args) {
+		
+		Player player = new Player();
+		
+		Einfuehrung einfuehrung = new Einfuehrung(); // Introduction instance
+		CharacterSelection charak = new CharacterSelection(); // Charakter selection
+		SwierigkeitsCharachter schwierig = new SwierigkeitsCharachter(); // Difficulty
+		Spiel01Wand spiel01 = new Spiel01Wand(); // Spiel 01
+		Spiel02Schwimm spiel02 = new Spiel02Schwimm(); // Spiel 02
+		//Spiel03 instance
+		Spiel04Weg weg = new Spiel04Weg();
+		Spiel05Seil spiel05 = new Spiel05Seil();
+		//Spiel06 instance
+		//Spiel07 instance
+		Ende ende = new Ende();
+		
+		//Intro
+		einfuehrung.intro();
+
+		// Character selection
+		charak.charakterAuswaehlen(player);
+		
+		// Difficulty
+		schwierig.difficulty(player);
+			player.save(); // saves hp and st values of the start, if player looses in game 
+		
+		// Spiel 01 Phuong Uyen To
+		spiel01.wand(player);
+			player.save(); // Saves hp and st values as they are when the player just ends this minigame.
+			
+		//Spiel 02 Marcello Melofiro
+		spiel02.schwimm(player);
+			player.save();
+			
+		// Spiel 03 Emir Sultanov
+			player.save();
+			
+		// Spiel 04 Phuong Uyen To
+		weg.weg(player);
+			player.save();
+			
+		// Spiel 05 Phuong Uyen To
+		spiel05.Seilschwingen(player);
+			player.save();
+			
+		// Spiel 06 Emir Sultanov
+			player.save();
+			
+		if (player.getSchwer() == true) {
+			
+			// Spiel 07 Emir Sultanov
+				player.save();
+			
+		}
+		
+		if (player.getGerettet() == true) {
+			
+			// Ende 2 Emir Sultanov
+			ende.ende2();
+			
+		} else {
+			
+			// Ende 1 Phuong Uyen To
+			ende.ende1();
+			
+		}
+
+	}
+
+}
