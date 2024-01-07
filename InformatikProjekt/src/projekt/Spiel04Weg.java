2ab102c8eee672ed18af9fab0866c433f360060f
uyen264 <154247339+uyen264@users.noreply.github.com>
2024-01-07 20:47:47 +0100

    Spiel04Weg.java

--- InformatikProjekt/src/projekt/Spiel04Weg.java
+++ InformatikProjekt/src/projekt/Spiel04Weg.java
@@ -26,11 +26,11 @@ public class Spiel04Weg {
 			int nummer1 = input.scannerInt("1. Trinken \n2. Essen", 2);
 			
 			if (nummer1 == 1) {
-				
+				System.out.println(" Nach dem Trinken stellen Sie 10 ST wieder her!");
 				player.setSt(player.getSt() + 10); // wenn er fur trinken wahlt, dann erhalt er ST
 				
 			} else if (nummer1 == 2) {// else erhalt er HP
-				
+				System.out.println(" Nach dem Essen stellen Sie 10  wieder her!");
 				player.setHp(player.getHp() + 10);
 				
 			}
@@ -39,34 +39,41 @@ public class Spiel04Weg {
 			
 			System.out.println("Sie werden von der Gruppe von Menschen angegriffen.");// und rechts
 			
-			System.out.println("Flucht oder sich schutzen?");
+			System.out.println("Mochten Sie fluchten oder sich schutzen und weiter gehen?");
 			
-			int nummer2 = input.scannerInt("1.Flucht \n2. Sich schutzen", 2);
+			int nummer2 = input.scannerInt("1.Fluchten \n2. Sich schutzen und weiter gehen", 2);
 			
 			if (nummer2 == 1) {// falls der Spieler flucht, dann wahlt er links und kann noch ST oder HP
 								// erhalten
-				System.out.println("Sie laufen schnell zurück zur Kreuzung und wählen links");
+				System.out.println("Sie laufen schnell zurück zur Kreuzung und gehen nach links");
 				
 				System.out.println("Sie finden ein Haus mit Essen und Getranke!");
-				
+				System.out.println("Mochten Sie essen oder trinken?");
 				int nummer3 = input.scannerInt("1. Trinken \n2. Essen", 2);// die Wahl des Spielers
 				
 				if (nummer3 == 1) {
-					
+					System.out.println(" Nach dem Trinken stellen Sie 10 ST wieder her!");
+
 					player.setSt(player.getSt() + 10); // wenn er fur trinken wahlt, dann erhalt er ST
 					
 				} else if (nummer3 == 2) {// else erhalt er HP
-					
+					System.out.println(" Nach dem Essen stellen Sie 10 HP wieder her!");
 					player.setHp(player.getHp() + 10);
 					
 				}
+				else {//sonst kann er die herausforderung uberwinden, aber verliert HP
+	                  	player.setHp(player.getHp() - 10);
+	                	if (player.getHp()<=0) {
+					weg(player);
+					System.out.println("Sie haben alle Lebenspunkte verloren.");
+				} else System.out.println("Sie sind ihnen erfolgreich entkommen, verlieren jedoch 10 HP.");
+			}
 				
 			}
-			// Ausgabe der aktuellen HP und ST
-			player.statusZeigen();
 			
 		}
-		
+			// Ausgabe der aktuellen HP und ST
+			player.statusZeigen();
 	}
 
 }
