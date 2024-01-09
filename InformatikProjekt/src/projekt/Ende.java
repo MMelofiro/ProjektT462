28abb8aa9b760dd819ee36640153e5038bf5a193
uyen264 <154247339+uyen264@users.noreply.github.com>
2024-01-09 23:52:54 +0100

    Ende.java

--- InformatikProjekt/src/projekt/Ende.java
+++ InformatikProjekt/src/projekt/Ende.java
@@ -2,21 +2,40 @@ package projekt;
 
 public class Ende {
 	
-	public void ende1() { // Phuong Uyen To
+	public void ende1(Player player) { // Phuong Uyen To
 		
-		System.out.println(
-				"Glückwunsch! Sie haben alle Herausforderungen des Spiels gemeistert und die\n"
-				+ "Organisation, die das Spiel kontrolliert, gefunden!");
-																								
+		System.out.println("Sie haben alle Herausforderungen des Spiels gemeistert und trifft den Spielveranstalter.");
+		System.out.println("Er gratuliert Ihnen und holt einen Koffer heraus, der aussieht, als ob darin ein Preisgeld enthalten wäre.");
+		input.next();
+		System.out.println("Aber dank der Erfahrung haben Sie erkannt, dass es sich um eine Waffe handelte, und weicht der Kugel aus.");
+		System.out.println("Sie sind jedoch immer noch von seinen Untergebenen umgeben.");
+		System.out.print("Als er sieht, dass Sie keinen Ausweg mehr haben, scheint er erleichtert zu sein und verrät, dass er weiß, dass Sie ein Detektiv sind");
+		System.out.println(", der gekommen ist, um Nachforschungen anzustellen und Ihnen alles über dieses Mordspiel zu erzählen.");
+		input.next();
+		System.out.println("Er verleitet Sie dazu, mit ihm zusammenzuarbeiten, weil er Ihre Fähigkeiten erkennt und verspricht, Ihnen viel Geld zu zahlen, wenn Sie kooperieren.");
+		System.out.println("Sie haben schon ein Signal an die Polizei gesendet und so getan, als würden Sie verhandeln, um Zeit zu gewinnen.");
+		input.next();
+		System.out.println("Die Organisation wurde verhaftet. Sie gedachten der Opfer und "+ player.getMitspieler()+" und erhielten große Belohnung.");
+																
 	}
 	
-	public void ende2() { // Emir Sultanov
+	public void ende2(Player player) { // Phuong Uyen To
 		
-		System.out.println(
-				"Sie haben alle Herausforderungen gemeistert, aber"+player.getMitspieler()+"\n"
-				+ " ist ein Mitglied der Organisation. Sie wurden gefangen genommen und\n"
-				+ "getötet.");
-		
+		System.out.println("Sie haben alle Herausforderungen gemeistert und trifft den Spielveranstalter.");
+		System.out.println("Er gratuliert Ihnen und holt einen Koffer heraus, der aussieht, als ob darin ein Preisgeld enthalten wäre.");
+		input.next();
+		System.out.println("Aber dank der Erfahrung haben Sie erkannt, dass es sich um eine Waffe handelte, und weicht der Kugel aus.");
+		System.out.println("Sie sind jedoch immer noch von seinen Untergebenen umgeben.");
+		System.out.print("Als er sieht, dass Sie keinen Ausweg mehr haben, scheint er erleichtert zu sein und verrät, dass er weiß, dass Sie ein Detektiv sind");
+		System.out.println(", der gekommen ist, um Nachforschungen anzustellen und Ihnen alles über dieses Mordspiel zu erzählen.");
+		input.next();
+		System.out.println("Sie möchten ein Signal an die Polizei senden und so tun, als würden Sie verhandeln, um Zeit zu gewinnen.");
+		input.next();
+		System.out.println("Aber "+player.getMitspieler()+"hat sich aufgehalten.");
+		System.out.println(player.getMitspieler()+" ist ein Mitglied der Organisation.");
+		System.out.println("Sie wurden getötet.");
+			
 	}
+
 
 }
